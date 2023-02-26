aec1fb901f3230b5c523c6b70163a7db0a090534
Gabrus Stanislav <stas.gabrus@gmail.com>
2023-02-26 20:45:07 +0300

    Added realization using functions with interim results

---
+++ UsingFunctions/lab8InterimAnswers.dpr
@@ -0,0 +1,200 @@
+Program lab8InterimAnswers;
+
+{$APPTYPE CONSOLE}
+
+uses
+  System.SysUtils;
+
+//Determining the type for matrixes
+Type
+  TMatrix = array [1..3, 1..3] of Integer;
+
+Var
+  A, B, C, D: TMatrix;
+  i, j: byte;
+  isCorrectInput: Boolean;
+
+  //A - first input matrix
+  //B - second input matrix
+  //C - matrix for interim and final result
+  //D - matrix for interim results
+  //i, j - iterators for the cycles
+  //isCorrectInput - boolean variable to check input for correctness
+
+//Procedure for input the matrixes
+Procedure Input(var a, b: TMatrix);
+Begin
+  writeln('You need to input integer matrixes and then the next expression will be calculated: 2 * (A + B) * (2 * B - A).');
+  writeln;
+  writeln('Enter the matrix A:');
+
+  //Repeating input matrix A until it's correct
+  repeat
+
+    //Making the input correct by default
+    isCorrectInput := true;
+
+    //Inputing the matrix A
+    for i := 1 to 3 do
+      for j := 1 to 3 do
+      begin
+
+        //Checking for exceptions
+        try
+          read(a[i][j]);
+        except
+          isCorrectInput := false;
+        end;
+
+      end;
+    writeln;
+
+    //Telling the user that input is incorrect
+    if not isCorrectInput then
+      writeln('Oops, your input is incorrect. Try again:');
+
+  until isCorrectInput;
+
+  writeln('Enter the matrix B:');
+
+  //Repeating input matrix B until it's correct
+  repeat
+
+    //Making the input correct by default
+    isCorrectInput := true;
+
+    //Inputing the matrix B
+    for i := 1 to 3 do
+      for j := 1 to 3 do
+      begin
+
+        //Checking for exceptions
+        try
+          read(b[i][j]);
+        except
+          isCorrectInput := false;
+        end;
+
+      end;
+    writeln;
+
+    //Telling the user that input is incorrect
+    if not isCorrectInput then
+      writeln('Oops, your input is incorrect. Try again:');
+
+  until isCorrectInput;
+End;
+
+//Procedure for summing two matrixes
+Function SumMatrixes (a, b: TMatrix): TMatrix;
+var
+  res: TMatrix;
+
+  //res - result of sum
+
+Begin
+  //Summing the matrixes
+  for i := 1 to 3 do
+    for j := 1 to 3 do
+      res[i][j] := a[i][j] + b[i][j];
+
+  result := res;
+End;
+
+//Procedure for substract two matrixes
+Function SubMatrixes (a, b: TMatrix): TMatrix;
+var
+  res: TMatrix;
+
+  //res - result of substract
+
+Begin
+  //Substract the matrixes
+  for i := 1 to 3 do
+    for j := 1 to 3 do
+      res[i][j] := a[i][j] - b[i][j];
+
+
+
+  result := res;
+End;
+
+//Procedure for multiplying number on matrix
+Function MultConstMatrix(Numb: byte; a: TMatrix): TMatrix;
+var
+  res: TMatrix;
+
+  //res - result of sum
+
+Begin
+  //Myltiplying number on matrix
+  for i := 1 to 3 do
+    for j := 1 to 3 do
+      res[i][j] := Numb * a[i][j];
+
+  result := res;
+End;
+
+//Procedure for multiplying matrixes
+Function MultMatrixes(a, b: TMatrix): TMatrix;
+var
+  res: TMatrix;
+  k: byte;
+
+  //res - result of sum
+  //k - another iterator for cycles
+
+Begin
+  //Myltiplying the matrixes
+  for i := 1 to 3 do
+    for j := 1 to 3 do
+    begin
+      res[i][j] := 0;
+      for k := 1 to 3 do
+        res[i][j] := res[i][j] + a[i][k] * b[k][j];
+    end;
+
+  result := res;
+End;
+
+//Procedure for outputting interim results
+Procedure Output(InterExpression: string; a: TMatrix);
+Begin
+  writeln(InterExpression);
+
+  for i := 1 to 3 do
+  begin
+    for j := 1 to 3 do
+      write(a[i][j]:10);
+    writeln;
+  end;
+  writeln;
+End;
+
+Begin
+  //Inputing the matrixes
+  Input(A, B);
+
+  //A + B
+  C := SumMatrixes(A, B);
+  Output('A + B =', C);
+
+  //2 * (A + B)
+  C := MultConstMatrix(2, C);
+  Output('2 * (A + B) = ', C);
+
+  //2 * B
+  D := MultConstMatrix(2, B);
+  Output('2 * B = ', D);
+
+  //2 * B - A
+  D := SubMatrixes(D, A);
+  Output('2 * B - A = ', D);
+
+  //2 * (A + B) * (2 * B - A)
+  C := MultMatrixes(C, D);
+  Output('2 * (A + B) * (2 * B - A) =', C);
+
+  readln;
+  readln;
+End.
